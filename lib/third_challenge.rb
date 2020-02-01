a63b16e456155fe5d183ba9fa5d956ee8013c0cb
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-01 03:50:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs 
-        
+        {name: "Lady Capulet", age: "51"},
         },
       :heroine => {
         #your key/value pairs here
